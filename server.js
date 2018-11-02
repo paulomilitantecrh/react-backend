0fa10313a2cad4fd1da1287f6bdabc0ae769bf76
Paulo Militante <p.militante@creativehope.jp>
2018-11-02 14:50:18 +0800

    Change server

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
+
 module.exports = app;
