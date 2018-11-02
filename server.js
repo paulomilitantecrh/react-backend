7883e58a632f4670884fbc213643bd17bdd8f5da
Paulo Militante <p.militante@creativehope.jp>
2018-11-02 12:13:26 +0800

    Updated static directory

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const app = require('./app');
 const path = require('path');
 const express = require('express');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
